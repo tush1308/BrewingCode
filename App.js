580f3e37a7998cf6a80613ccb8b37c48dd76c9f7
Tushar <tusharrahuljain@gmail.com>
2021-12-18 15:41:40 +0530

    Created navigation

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { View,Text } from 'react-native';
+import Login from './src/screens/Login';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
 
 const App=()=> {
 
   return (
-    <View>
-      <Text>Hi</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name='Login' component={Login} options={{headerShown:false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
