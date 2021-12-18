365c3ddb3bb4d739abd6e21770c10e47fe9de9aa
Arpit472002 <sarpit623@gmail.com>
2021-12-18 14:19:38 +0530

    Added Auth

--- main/models.py
+++ main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from login_signup.models import MyUser
 # Create your models here.
+class ItemType(models.Model):
+    item_category_id=models.AutoField(primary_key=True)
+    item_category=models.CharField(max_length=200)
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.item_category
+
+class Item(models.Model):
+    item_id=models.AutoField(primary_key=True)
+    item_type=models.ForeignKey(ItemType,on_delete=models.CASCADE)
+    item_name=models.CharField(max_length=200)
+    item_brand=models.CharField(max_length=100)
+    item_price=models.DecimalField()
+    available_quantity=models.IntegerField(null=False,blank=False)
+    owned_by=models.ForeignKey(MyUser.is_superuser,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.item_name
+
+class Order(models.Model):
+    order_id=models.AutoField(primary_key=True)
+    user=models.ForeignKey(MyUser,on_delete=models.CASCADE)
+    product=models.ForeignKey(Item,on_delete=models.CASCADE)
