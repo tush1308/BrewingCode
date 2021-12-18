bd584247dd9fb602a8f50f5544e2c56d75c981d3
Sanika <sanikaardekar@gmail.com>
2021-12-18 14:52:47 +0530

    test commit

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -11,7 +11,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-
+//signup
 const theme = createTheme();
 
 export default function SignUp() {
