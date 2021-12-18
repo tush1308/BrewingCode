7226c7cdd54b6c08d0529058fa2d6bccb5b2ac3b
Arpit472002 <sarpit623@gmail.com>
2021-12-18 15:34:48 +0530

    Allowed all using cors

--- main/models.py
+++ main/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from login_signup.models import MyUser
 # Create your models here.
+PAYMENT_METHOD=(
+    ('CASH ON DELIVERY','CASH ON DELIVERY'),
+    ('CREDIT CARD','CREDIT CARD'),
+    ('NETBANKING','NETBANKING'),
+    ('UPI','UPI')
+)
 class ItemType(models.Model):
     item_category_id=models.AutoField(primary_key=True)
     item_category=models.CharField(max_length=200)
@@ -24,3 +30,11 @@ class Order(models.Model):
     order_id=models.AutoField(primary_key=True)
     user=models.ForeignKey(MyUser,on_delete=models.CASCADE)
     product=models.ForeignKey(Item,on_delete=models.CASCADE)
+    total_bill=models.IntegerField()
+    payment_method=models.CharField(max_length=100,choices=PAYMENT_METHOD)
+    address=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.user
+
+
--- main/views.py
+++ main/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import *
+from .serializers import *
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+class OrderItemType(generics.ListAPIView):
+    queryset=ItemType.objects.all()
+    serializer_class=OrderItemTypeSerializer
+    permission_classes=IsAuthenticated
+
+class OrderItemType_detail(generics.RetrieveAPIView):
+    queryset=ItemType.objects.all()
+    serializer_class=OrderItemTypeSerializer
+    permission_classes=IsAuthenticated
+
+class OrderItem(generics.ListAPIView):
+    queryset=Item.objects.all()
+    serializer_class=OrderItemSerializer
+    permission_classes=IsAuthenticated
+
+class OrderItem_detail(generics.RetrieveAPIView):
+    queryset=Item.objects.all()
+    serializer_class=OrderItemSerializer
+    permission_classes=IsAuthenticated
+
