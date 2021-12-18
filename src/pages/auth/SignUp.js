fd0a1fb35e442bd6d18de83b60594ef17a277d96
Sanika <sanikaardekar@gmail.com>
2021-12-18 14:48:41 +0530

    navbar added

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -1,167 +1,163 @@
-import * as React from 'react';
-import { useState } from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import {Link} from 'react-router-dom';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme();
 
 export default function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isLoading, setLoading] = useState(true);
 
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
-const [firstName, setFirstName] = useState("");
-const [lastName, setLastName] = useState("");
-const [isLoading, setLoading] = useState(true);
-
-const cors = require('cors');
+  const cors = require("cors");
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
     createacc();
   };
 
   async function createacc() {
-console.log("hello");
-try{
-  
-    let result = await fetch('https://rats-hackathon.herokuapp.com/login-signup/register/', {
-      method: 'POST',
-      body: JSON.stringify(
+    console.log("hello");
+    try {
+      let result = await fetch(
+        "https://rats-hackathon.herokuapp.com/login-signup/register/",
         {
-          "email":email,
-          "first_name":firstName,
-          "last_name":lastName,
-          "password":password
+          method: "POST",
+          body: JSON.stringify({
+            email: email,
+            first_name: firstName,
+            last_name: lastName,
+            password: password,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
         }
-      ),
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-    });
-    result = await result.json();
-    console.log(result);
-  }catch(error){
-    console.log("Error"+error);
-  }finally{
-    setLoading(false);
-  }
+      );
+      result = await result.json();
+      console.log(result);
+    } catch (error) {
+      console.log("Error" + error);
+    } finally {
+      setLoading(false);
+    }
   }
   return (
-    <div style={{backgroundColor:'#90B8F8'}}>
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
-          
-            <Grid container spacing={2}>
-            
-              <Grid item xs={12} sm={6}>
-                
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                  value={firstName.trim()}
-              onChange={(e) => setFirstName(e.target.value)}
-                  style={{backgroundColor:'white'}}
-                />
+    <div style={{ backgroundColor: "#90B8F8" }}>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                    value={firstName.trim()}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    style={{ backgroundColor: "white" }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    value={lastName.trim()}
+                    onChange={(e) => setLastName(e.target.value)}
+                    autoComplete="family-name"
+                    style={{ backgroundColor: "white" }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    value={email.trim()}
+                    onChange={(e) => setEmail(e.target.value)}
+                    autoComplete="email"
+                    style={{ backgroundColor: "white" }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                    value={password.trim()}
+                    onChange={(e) => setPassword(e.target.value)}
+                    style={{ backgroundColor: "white" }}
+                  />
+                </Grid>
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  value={lastName.trim()}
-              onChange={(e) => setLastName(e.target.value)}
-                  autoComplete="family-name"
-                  style={{backgroundColor:'white'}}
-                />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                style={{ backgroundColor: "#353941" }}
+                //onSubmit={createacc}
+              >
+                Sign Up
+              </Button>
+
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link to="/" variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  value={email.trim()}
-              onChange={(e) => setEmail(e.target.value)}
-                  autoComplete="email"
-                  style={{backgroundColor:'white'}}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  value={password.trim()}
-              onChange={(e) => setPassword(e.target.value)}
-                  style={{backgroundColor:'white'}}
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              style={{backgroundColor:'#353941'}}
-               //onSubmit={createacc}
-            >
-              Sign Up
-            </Button>
-            
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link to="/" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
+            </Box>
           </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
