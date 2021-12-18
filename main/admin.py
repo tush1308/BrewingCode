aa40ee711c455cd0d4abd8a975d03a8c7a01d0f4
Arpit472002 <sarpit623@gmail.com>
2021-12-18 17:36:57 +0530

    Updated Admin Interface

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+@admin.register(ItemType)
+class ItemTypeModelAdmin(admin.ModelAdmin):
+    list_display=['item_category_id','item_category','created_at']
+
+@admin.register(Item)
+class ItemTypeModelAdmin(admin.ModelAdmin):
+    list_display=['item_id','item_name','item_brand','item_price','available_quantity','owned_by','created_at']
+
+@admin.register(Order)
+class ItemTypeModelAdmin(admin.ModelAdmin):
+    list_display=['order_id','user','product','total_bill','payment_method','created_at']
--- main/models.py
+++ main/models.py
@@ -35,6 +35,6 @@ class Order(models.Model):
     address=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     def __str__(self):
-        return self.user
+        return str(self.user)
 
 
