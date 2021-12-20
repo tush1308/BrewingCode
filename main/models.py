cb9a16ef625c20655362cb948068f41feddf7b53
Arpit472002 <sarpit623@gmail.com>
2021-12-20 18:52:28 +0530

    Added discount formula

--- main/models.py
+++ main/models.py
@@ -31,27 +31,46 @@ class OrderItem(models.Model):
     cart_item=models.ForeignKey(Item,on_delete=models.CASCADE,null=True,blank=True)
     price=models.IntegerField(null=True,blank=True)
     quantity=models.IntegerField(default=1)
-    total_price=models.IntegerField(null=True,blank=True)
+    discount=models.DecimalField(max_digits=9, decimal_places=2,null=True,blank=True)
+    total_price=models.DecimalField(max_digits=9, decimal_places=2,null=True,blank=True)
     created_by=models.ForeignKey(MyUser,on_delete=models.CASCADE)
     def __str__(self):
         return '%s %s' % (self.cart_item,self.quantity)
+ #Calculating Discount
+    def calc_disc(self):
+        if self.quantity<10:
+            self.discount=0
+        elif self.quantity>=10 and self.quantity<20:
+            self.discount=self.price*self.quantity*0.05
+        elif self.quantity>=20 and self.quantity<40:
+            self.discount=self.price*self.quantity*0.1
+        elif self.quantity>=40 and self.quantity<60:
+            self.discount=self.price*self.quantity*0.2
+        elif self.quantity>=60:
+            self.discount=self.price*self.quantity*0.3
+        return self.discount
+
     def save(self,*args,**kwargs):
         self.price=self.cart_item.item_price
-        self.total_price=self.price*self.quantity
+        self.discount=self.calc_disc()
+        self.total_price=self.price*self.quantity-self.discount
         super(OrderItem,self).save(*args,**kwargs)
+
+
 
 
 class Order(models.Model):
     order_id=models.AutoField(primary_key=True)
     user=models.ForeignKey(MyUser,on_delete=models.CASCADE)
     items_added_in_cart=models.ManyToManyField(OrderItem)
-    total_bill=models.IntegerField(null=True,blank=True)
+    total_bill=models.DecimalField(max_digits=9, decimal_places=2,null=True,blank=True)
     payment_method=models.CharField(max_length=100,choices=PAYMENT_METHOD)
     address=models.TextField()
     city=models.CharField(max_length=50,null=False,blank=False)
     created_at=models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return str(self.payment_method)
+
 
     
 
--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('order/',OrderView.as_view(),name='order'),
     path('order_detail/<int:pk>/',OrderView_detail.as_view(),name='order_detail'),
     path('ordered_item/',OrderedItemView.as_view(),name='ordered_item'),
-    path('ordered_item_detail/',OrderedItemView_detail.as_view(),name='ordered_item_detail')
+    path('ordered_item_detail/<int:pk>',OrderedItemView_detail.as_view(),name='ordered_item_detail')
 ]
